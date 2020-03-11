dc439fe29d06cff39969db06342203e66fce64e3
PSlagveer <patric.slagveer@priva.com>
2020-03-12 00:44:18 +0100

    Update App

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -11,7 +11,10 @@ import { EarningsModule } from './earnings/earnings.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/Schoolbudgetten'),
+    // MongooseModule.forRoot('mongodb://localhost:27017/Schoolbudgetten'),
+    MongooseModule.forRoot(
+      'mongodb://patric:patricOne@clusterslagveer-shard-00-00-e8lpw.azure.mongodb.net:27017,clusterslagveer-shard-00-01-e8lpw.azure.mongodb.net:27017,clusterslagveer-shard-00-02-e8lpw.azure.mongodb.net:27017/Schoolbudget?ssl=true&replicaSet=ClusterSlagveer-shard-0&authSource=admin&retryWrites=true&w=majority'
+    ),
     StudentsModule,
     BudgetsModule,
     ReceiptsModule,
